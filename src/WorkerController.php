c70b3346d462c6b6305b44669b57d53501adc7df
MarcA098 <168072534+MarcA098@users.noreply.github.com>
2024-11-27 20:15:05 +0100

    Issue #3, #4 & #5

--- src/WorkerController.php
+++ src/WorkerController.php
@@ -37,19 +37,23 @@ try {
 
     // Create operation
     $sql_sentence = "INSERT INTO worker(dni,name,surname,salary,phone) 
-            VALUES('71111111D','Juan','González',20000,'93500202')";
+            VALUES('81111111D','Juan','González',20000,'93500202')";
 
     try {
         $result = $mysqli->query($sql_sentence);
         // write info message with "Record inserted successfully"
-        //TODO
+        if($result) {
+            $log->info("Record inserted successfully");
+        }else{
+            $log->error("Error inserting a record: " . $mysqli->error);
+        }
         ++$steps;
     } catch (mysqli_sql_exception $e) {
         //  write error message with "Error inserting a record"
-        //TODO
+        $log->error("Error inserting a record: " . $e->getMessage());
     }
 } catch (mysqli_sql_exception $e) {
     //  write error message with "Error connection db: + details parameters config"
-    //TODO
+    $log->error("Error connection db: " . $e->getMessage());
 }
 echo "steps executed correctly: " . $steps;
